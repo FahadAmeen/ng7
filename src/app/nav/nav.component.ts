0faeeb8b447a823754455fa245d9baf5b556beff
Muhammad Fahad Ameen <mfah@ciklum.com>
2019-06-25 11:03:18 +0500

    change color to black

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  appTitle:string='dynamic name';
+  appTitle:string='ng7';
   
   constructor() { }
 
